fb2eb7ab346cdfd247a1a100de1d7852182fa40e
Alexandro Delgado <82964108+adelgadoj@users.noreply.github.com>
2024-01-21 08:08:44 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = new mysqli("localhost", "id20053745_admin", "ssVn3+4H2e^*-(NA", "id20053745_teatro_unsa" , "3306");
+    $conexion = new mysqli("localhost", "user", "password", "name_bd" , "3306");
     $conexion->set_charset("utf8");
 ?>
